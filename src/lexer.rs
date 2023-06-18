af6994ca5628ac759c1e7d0bd12ba2528d0b7922
Paul Zupan <paul.zupan.13@gmail.com>
2023-06-17 17:53:26 -0700

    Implement symbols in lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,20 +1,18 @@
 #[derive(Debug)]
 pub enum Token {
     // Single character
-    Minus,
     ParenLeft,
     ParenRight,
-    Plus,
-    Slash,
-    Star,
 
     // Multiple characters
-    Number(u64),
+    Number(String),
+    Symbol(String),
 }
 
 struct Lexer {
     current: usize,
     source: Vec<u8>,
+    start: usize,
 }
 
 impl Lexer {
@@ -22,6 +20,7 @@ impl Lexer {
         Self {
             current: 0,
             source: source.into_bytes(),
+            start: 0,
         }
     }
 
@@ -39,8 +38,22 @@ impl Lexer {
         byte
     }
 
-    fn next_number(&mut self) -> Token {
+    fn next_number(&mut self) -> String {
+        while let Some(byte) = self.peek_byte() {
+            if !Self::is_numeric(byte) {
+                break;
+            }
+            self.next_byte();
+        }
+        String::from_utf8_lossy(&self.source[self.start..self.current]).to_string()
+    }
+
+    fn next_symbol(&mut self) -> String {
         todo!()
+    }
+
+    fn peek_byte(&self) -> Option<u8> {
+        self.source.get(self.current).copied()
     }
 }
 
@@ -48,16 +61,13 @@ impl Iterator for Lexer {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
+        self.start = self.current;
         match self.next_byte()? {
-            b'-' => Some(Token::Minus),
             b'(' => Some(Token::ParenLeft),
             b')' => Some(Token::ParenRight),
-            b'+' => Some(Token::Plus),
-            b'/' => Some(Token::Slash),
-            b'*' => Some(Token::Star),
-            byte if Self::is_numeric(byte) => Some(self.next_number()),
+            byte if Self::is_numeric(byte) => Some(Token::Number(self.next_number())),
             byte if Self::is_whitespace(byte) => self.next(),
-            byte => panic!("unrecognized byte '{}'", byte as char),
+            _ => Some(Token::Symbol(self.next_symbol())),
         }
     }
 }
